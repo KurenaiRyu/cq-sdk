0b65d93faa2094ffccebcf3073d604874f170f5d
Kurenai <kurenai233@yahoo.com>
2021-09-18 15:28:03 +0800

    Initial commit

---
+++ src/main/java/io/github/kurenairyu/cqsdk/pojo/Bot.java
@@ -0,0 +1,23 @@
+package io.github.kurenairyu.cqsdk.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Kurenai
+ * @since 2021-04-02 14:24
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Bot {
+
+    private long   qq;
+    private String nickname;
+    private String xClientRole;
+
+}
---
+++ src/main/java/io/github/kurenairyu/cqsdk/pojo/result/ActionResult.java
@@ -0,0 +1,23 @@
+package io.github.kurenairyu.cqsdk.pojo.result;
+
+import lombok.Data;
+import org.kurenai.qqbot.constant.ResponseStatus;
+
+/**
+ * @author Kurenai
+ * @since 2021-04-09 15:30
+ */
+@Data
+public class ActionResult {
+
+    private Data           data;
+    private String         echo;
+    private int            retcode;
+    private ResponseStatus status;
+
+    @lombok.Data
+    public static class Data {
+        private int messageId;
+    }
+
+}
